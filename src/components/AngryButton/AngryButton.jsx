cbcdf860108f20d896f16b628179696fed9f866a
Atkinss1 <jordanatkins@live.com>
2024-01-23 21:39:32 -0700

    add useState to component

--- src/components/AngryButton/AngryButton.jsx
+++ src/components/AngryButton/AngryButton.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 
 function AngryButton(props) {
+  const [anger, setAnger] = useState(0);
 
+  const increaseAnger = () => {
+    if (anger >= 1) {
+      anger = anger + 0.1
+    } else {
+      setAnger(0);
+    }
+  }
   return (
-    <button className="AngryButton">
-    {/* When the threshold is not reached */}<span>Don't click me too much! </span>
-    {/* When the threshold is not reached */}<span>Rawr!</span>
+    <button className="AngryButton" style={{backgroundColor: 
+    `rgba(255,0,0,${anger})`}}>
+    {anger > 1 ? <span>Rawr!</span> : <span>Don't click me too much! </span>}
   </button>
   );
 }
