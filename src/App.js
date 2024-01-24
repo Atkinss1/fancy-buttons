bfe499b8f9516a6ded1399a53b44327ef74cbc81
Atkinss1 <jordanatkins@live.com>
2024-01-23 21:02:24 -0700

    add: sub-components to app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import AngryButton from '../src/components/AngryButton/AngryButton';
+import CounterButton from '../src/components/CounterButton/CounterButton';
+import LightSwitchButton from '../src/components/LightSwitchButton/LightSwitchButton';
+import TextRepeaterButton from '../src/components/TextRepeaterButton/TextRepeaterButton';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"App"}>
+      <section>
+        <AngryButton />
+        <CounterButton />
+        <LightSwitchButton />
+        <TextRepeaterButton />
+      </section>
     </div>
   );
 }
