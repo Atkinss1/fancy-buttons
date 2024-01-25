c9bb40bb5b5d0d82e6a356fcf45e48f8d13646ee
Atkinss1 <jordanatkins@live.com>
2024-01-24 19:41:38 -0700

    refactor: pass down state and switchLight function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import './App.css';
+import { useState } from 'react';
 import AngryButton from '../src/components/AngryButton/AngryButton';
 import CounterButton from '../src/components/CounterButton/CounterButton';
 import LightSwitchButton from '../src/components/LightSwitchButton/LightSwitchButton';
 import TextRepeaterButton from '../src/components/TextRepeaterButton/TextRepeaterButton';
 
+
 function App() {
-  return (
-    <div className={"App"}>
+
+    const [light, setLight] = useState('off');
+    const dark = (light === 'off') ? 'dark' : '';
+
+    const switchLight = () => {
+      setLight((light === 'on') ? 'off' : 'on');
+    }
+  
+    return (
+    <div className={`App ${dark}`}>
       <section>
         <AngryButton />
         <CounterButton />
-        <LightSwitchButton />
+        <LightSwitchButton light={light} switchLight={switchLight} />
         <TextRepeaterButton />
       </section>
     </div>
